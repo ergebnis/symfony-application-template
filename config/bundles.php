a4845ba7cc5757ed5927b978490e365cc6fc68a3
Andreas Möller <am@localheinz.com>
2020-05-21 19:09:01 +0200

    Fix: Require sensio/framework-extra-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -12,5 +12,6 @@ declare(strict_types=1);
  */
 
 return [
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
 ];
