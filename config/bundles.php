b1cda44054343c28d8932bdeb0ec1e48810f6816
Andreas Möller <am@localheinz.com>
2020-08-09 10:08:05 +0200

    Enhancement: Require doctrine/doctrine-migrations-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -13,6 +13,7 @@ declare(strict_types=1);
 
 return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
 ];
