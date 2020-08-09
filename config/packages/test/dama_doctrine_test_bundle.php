178a462744359365b3d44496ce795d405714131a
Andreas Möller <am@localheinz.com>
2020-08-09 13:29:05 +0200

    Enhancement: Require dama/doctrine-test-bundle

---
+++ config/packages/test/dama_doctrine_test_bundle.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Copyright (c) 2020 Andreas Möller
+ *
+ * For the full copyright and license information, please view
+ * the LICENSE.md file that was distributed with this source code.
+ *
+ * @see https://github.com/ergebnis/symfony-application-template
+ */
+
+use Symfony\Component\DependencyInjection;
+
+return static function (DependencyInjection\Loader\Configurator\ContainerConfigurator $containerConfigurator): void {
+    $containerConfigurator->extension('dama_doctrine_test', [
+        'enable_static_connection' => true,
+        'enable_static_meta_data_cache' => true,
+        'enable_static_query_cache' => true,
+    ]);
+};
