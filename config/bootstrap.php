011696cde12fd6d0bed54569adfb498bafccf211
Andreas Möller <am@localheinz.com>
2020-09-20 18:37:35 +0200

    Enhancement: Rename variable

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -17,10 +17,10 @@ require __DIR__ . '/../vendor/autoload.php';
 
 // Load cached env vars if the .env.local.php file exists
 // Run "composer dump-env prod" to create it (requires symfony/flex >=1.2)
-$env = include __DIR__ . '/../.env.local.php';
+$environmentVariables = include __DIR__ . '/../.env.local.php';
 
-if (\is_array($env) && (!isset($env['APP_ENV']) || ($_SERVER['APP_ENV'] ?? $_ENV['APP_ENV'] ?? $env['APP_ENV']) === $env['APP_ENV'])) {
-    foreach ($env as $k => $v) {
+if (\is_array($environmentVariables) && (!isset($environmentVariables['APP_ENV']) || ($_SERVER['APP_ENV'] ?? $_ENV['APP_ENV'] ?? $environmentVariables['APP_ENV']) === $environmentVariables['APP_ENV'])) {
+    foreach ($environmentVariables as $k => $v) {
         $_ENV[$k] = $_ENV[$k] ?? (isset($_SERVER[$k]) && 0 !== \mb_strpos($k, 'HTTP_') ? $_SERVER[$k] : $v);
     }
 } elseif (!\class_exists(Dotenv::class)) {
