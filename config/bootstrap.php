78974eb44d7756fc4d80fb4363bcb349e6ca953e
Andreas Möller <am@localheinz.com>
2020-09-20 18:36:39 +0200

    Fix: Remove assignment from condition

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -17,7 +17,9 @@ require __DIR__ . '/../vendor/autoload.php';
 
 // Load cached env vars if the .env.local.php file exists
 // Run "composer dump-env prod" to create it (requires symfony/flex >=1.2)
-if (\is_array($env = include __DIR__ . '/../.env.local.php') && (!isset($env['APP_ENV']) || ($_SERVER['APP_ENV'] ?? $_ENV['APP_ENV'] ?? $env['APP_ENV']) === $env['APP_ENV'])) {
+$env = include __DIR__ . '/../.env.local.php';
+
+if (\is_array($env) && (!isset($env['APP_ENV']) || ($_SERVER['APP_ENV'] ?? $_ENV['APP_ENV'] ?? $env['APP_ENV']) === $env['APP_ENV'])) {
     foreach ($env as $k => $v) {
         $_ENV[$k] = $_ENV[$k] ?? (isset($_SERVER[$k]) && 0 !== \mb_strpos($k, 'HTTP_') ? $_SERVER[$k] : $v);
     }
