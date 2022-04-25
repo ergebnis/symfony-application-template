998d44e30045555e32da23e675d83027339f2bd6
Andreas Möller <am@localheinz.com>
2022-04-25 17:36:24 +0200

    Enhancement: Use symfony/runtime

--- public/index.php
+++ public/index.php
@@ -12,30 +12,12 @@ declare(strict_types=1);
  */
 
 use App\Kernel;
-use Symfony\Component\Dotenv\Dotenv;
-use Symfony\Component\ErrorHandler\Debug;
-use Symfony\Component\HttpFoundation\Request;
 
-require \dirname(__DIR__) . '/vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload_runtime.php';
 
-(new Dotenv())->bootEnv(\dirname(__DIR__) . '/.env');
-
-if ($_SERVER['APP_DEBUG']) {
-    \umask(0);
-
-    Debug::enable();
-}
-
-if ($trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? false) {
-    Request::setTrustedProxies(\explode(',', $trustedProxies), Request::HEADER_X_FORWARDED_FOR | Request::HEADER_X_FORWARDED_PORT | Request::HEADER_X_FORWARDED_PROTO);
-}
-
-if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? false) {
-    Request::setTrustedHosts([$trustedHosts]);
-}
-
-$kernel = new Kernel($_SERVER['APP_ENV'], (bool) $_SERVER['APP_DEBUG']);
-$request = Request::createFromGlobals();
-$response = $kernel->handle($request);
-$response->send();
-$kernel->terminate($request, $response);
+return static function (array $context): Kernel {
+    return new Kernel(
+        $context['APP_ENV'],
+        (bool) $context['APP_DEBUG'],
+    );
+};
