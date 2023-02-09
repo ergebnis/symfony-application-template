cd1669994504294172562c160de9003ac2a8ee1f
Andreas Möller <am@localheinz.com>
2023-02-09 23:38:47 +0100

    Fix: Remove sensio/framework-extra-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -15,6 +15,5 @@ return [
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
-    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
 ];
