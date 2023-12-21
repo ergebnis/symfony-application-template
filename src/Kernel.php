0c71b1e4218eed4f9848a5637acd0674880be336
Andreas Möller <am@localheinz.com>
2023-12-21 18:29:15 +0100

    Fix: Remove unnecessary code

--- src/Kernel.php
+++ src/Kernel.php
@@ -19,16 +19,6 @@ use Symfony\Component\HttpKernel;
 final class Kernel extends HttpKernel\Kernel
 {
     use FrameworkBundle\Kernel\MicroKernelTrait;
-    public function registerBundles(): iterable
-    {
-        $contents = require $this->getProjectDir() . '/config/bundles.php';
-
-        foreach ($contents as $class => $envs) {
-            if ($envs[$this->environment] ?? $envs['all'] ?? false) {
-                yield new $class();
-            }
-        }
-    }
 
     public function getProjectDir(): string
     {
